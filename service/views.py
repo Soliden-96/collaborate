6f33dc0543bf90acf3c323f8fc7c6c55e59d0846
dengidengi <dengianassi@gmail.com>
2023-11-19 18:54:07 +0100

    before collecting static

--- service/views.py
+++ service/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def base(request):
-    render(request,'service/base.html')
+    return render(request,'service/base.html')
--- vite-setup/vite.config.js
+++ vite-setup/vite.config.js
@@ -4,12 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/static",
+  base:"static",
   build:{
     manifest:true,
-    outDir:'./src/assets',
+    outDir:'./vite-setup/src/dist',
     rollupOptions:{
-      input:'./src/main.jsx'
+      input:{
+        base:'./src/main.jsx'
+      },
     },
   },
 })
